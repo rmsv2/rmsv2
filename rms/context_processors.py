9acb53d56ed9c1471f817754eaa360ef9913f1a2
Julian Jacobi <mail@julianjacobi.net>
2018-04-05 08:33:39 +0200

    fix reservation menu notification

--- rms/context_processors.py
+++ rms/context_processors.py
@@ -10,7 +10,9 @@ def categories(request):
 
 def menu_notifications(request):
     if request.user.is_authenticated:
-        danger_reservations = models.Reservation.objects.filter(owners=request.user, end_date__lt=timezone.now()).count()
+        danger_reservations = models.Reservation.objects\
+            .filter(owners=request.user, end_date__lt=timezone.localtime(timezone.now()))\
+            .exclude(reservationcheckoutinstance=None).count()
         return {
             'danger_reservations_count': danger_reservations
         }
